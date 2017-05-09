a469ea2799a288c7b2edb82a06678387021df516
jain.gaurav@gmail.com <jain.gaurav@gmail.com>
2017-05-08 22:31:38 -0400

    Modified ecn-new

--- src/app/ecn-new/ecn-new.component.ts
+++ src/app/ecn-new/ecn-new.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Iecn} from '../ecn/Iecn'
 
 @Component({
-  selector: 'app-ecn-new',
+  selector: 'ecn-new',
   templateUrl: './ecn-new.component.html',
   styleUrls: ['./ecn-new.component.css']
 })
@@ -9,7 +10,10 @@ export class EcnNewComponent implements OnInit {
 
   constructor() { }
 
+  ecn: Iecn
+
   ngOnInit() {
+    this.ecn =  {ecnNo: "E123456", description: "typical ecn", status: "CCB", priority: 1, resource: "Gaurav", comments: ["Nothing"]};
   }
 
 }
