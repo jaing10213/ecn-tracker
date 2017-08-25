0fab6128b7ca44a43652c6508988feb82eed6a71
jain.gaurav@gmail.com <jain.gaurav@gmail.com>
2017-08-24 23:23:24 -0400

    improved search

--- src/app/ecn-list/ecn-filter.pipe.ts
+++ src/app/ecn-list/ecn-filter.pipe.ts
@@ -11,9 +11,11 @@ export class EcnFilterPipe implements PipeTransform {
    
     return (value==null)?value:value.filter( 
       ecn =>  (
-                ((ecn.status==null)?false:ecn.status.toLowerCase().indexOf(param1.toLowerCase())>=0)
+                 ((ecn.status==null)?false:ecn.status.toLowerCase().indexOf(param1.toLowerCase())>=0)
               || ((ecn.ecnNo==null)?false:ecn.ecnNo.toLowerCase().indexOf(param1.toLowerCase())>=0)
               || ((ecn.resource==null)?false:ecn.resource.toLowerCase().indexOf(param1.toLowerCase())>=0)
+              || ((ecn.description==null)?false:ecn.description.toLowerCase().indexOf(param1.toLowerCase())>=0)
+              || ((ecn.comments==null)?false:ecn.comments.some(a=>a.value.toLowerCase().indexOf(param1.toLowerCase())>=0))
             ));
     
   }
